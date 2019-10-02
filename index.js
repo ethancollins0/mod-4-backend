ca78089a183f204a4e2605d899c19cf6bcb75854
Ethan Collins <ethantcollins98@gmail.com>
2019-10-02 10:15:18 -0600

    wip

--- index.js
+++ index.js
@@ -8,11 +8,15 @@ const jwt = require('jsonwebtoken')
 
 app.use(bodyParser.urlencoded())
 app.use(bodyParser.json())
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", '*');
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+  next();
+});
 app.use(cors())
-app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", '*')
-    next();
-})
+
 
 app.post('/home', validateToken, (req, res) => {
     jwt.verify(req.token, process.env.SECRET, (err, decoded) => {
