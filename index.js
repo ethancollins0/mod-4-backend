4015d15cb9b2f4ffe2a585d73246a8974e7a0575
Ethan Collins <ethantcollins98@gmail.com>
2019-10-02 10:12:22 -0600

    wip

--- index.js
+++ index.js
@@ -2,14 +2,17 @@ const express = require('express')
 const cors = require('cors')
 const bodyParser = require('body-parser')
 const app = express()
-
-app.use(cors())
-app.use(bodyParser.urlencoded())
-app.use(bodyParser.json())
-
 const db = require('./database_connection')
 const auth = require('./authenticate_company')
 const jwt = require('jsonwebtoken')
+
+app.use(bodyParser.urlencoded())
+app.use(bodyParser.json())
+app.use(cors())
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", '*')
+    next();
+})
 
 app.post('/home', validateToken, (req, res) => {
     jwt.verify(req.token, process.env.SECRET, (err, decoded) => {
